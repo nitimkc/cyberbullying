ada1eb057760a670b9b5e1420f8f313b58ffdf48
nitimkc <nitimkc@gmail.com>
2019-07-25 16:08:05 -0400

    added some files

--- python/twitter_credentials.py
+++ python/twitter_credentials.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-consumerkey="78LE0dw99xMMHfJ1eCgQyQH7v"
-consumersecret="71K8pclxCzTWTkKWuVqSEDUcUOSDgloUDaclR1e1ho6S1rHT1U"
-accesstoken="256116632-uffhXDh2asQgzt1Pq5Ul9pW1ovLelJoJ0xKGjHUV"
-accesssecret="k04WguWtG8wjqsLk5Bqo5GdbloWXmfRkxwDyMEHLJGjuL"
-
-=======
 consumerkey="v4rArqwixhgQdDNpSyZSxfvnw"
 consumersecret="LlfGhNOquKHxTM5fNC4Amd8bAET3yGuARC81xdkA7QRbwZYVq1"
 accesstoken="256116632-N3NOGc6sspTmI9z8OAilJRwBG89WxCQPSmnICVQW"
 accesssecret="PIB1MqMSYqmEkiGOt3a0SkpZrBO9CdBUpv4D53uPBVZ7V"
->>>>>>> af8b368477a16e6a3b111f0da11060d214ecf903
